1300a74d31e22b73dbd12f4fe376a9053124cdc8
rehanmiah <rehan_miah@hotmail.com>
2021-01-24 16:40:04 +0000

    Update app.js

--- api/app.js
+++ api/app.js
@@ -3,4 +3,14 @@ const path = require('path');
 
 const app = express();
 
+const pathToIndex = path.resolve(__dirname, '../client/index.html');
+
+app.use('/*', (request, response) => {
+    response.sendFile(pathToIndex);
+});
+
+
+
+inex.html;
+const path.resolve() 
 module.exports = app;
