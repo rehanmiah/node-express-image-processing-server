7f5de0a924d4ce21eac34b0366c5b7830cd93c63
rehanmiah <rehan_miah@hotmail.com>
2021-01-24 17:58:16 +0000

    Update router.js

--- api/src/router.js
+++ api/src/router.js
@@ -15,3 +15,12 @@ router=Router();
 module.exports = router;
 
 
+const = fileFilter(request, file, callback) => {
+    if (file.mimetype != 'image.png'){
+        request.fileValidationError='Wrong file type';
+        callback(null,false,Error)
+        }
+    else{
+        callbuck(null,True)
+    }
+}
