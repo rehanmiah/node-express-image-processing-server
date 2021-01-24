805a7d71c11f73b4f069408647d7928b540a2ade
rehanmiah <rehan_miah@hotmail.com>
2021-01-24 17:46:07 +0000

    Update router.js

--- api/src/router.js
+++ api/src/router.js
@@ -2,15 +2,6 @@ const {Router} = require('express');
 const { diskStorage } = require('multer');
 const multer = require('multer');
 
-const storage = multer.diskStorage({
-    destination: 'api/uploads/',
-    filename,
-});
-
-router=Router();
-module.exports = router;
-
-
 const filename = (request, file, callback) => {
     callback(null, file.originalname);
   };
@@ -19,3 +10,8 @@ const filename = (request, file, callback) => {
     destination: 'api/uploads/',
     filename,
   });
+
+router=Router();
+module.exports = router;
+
+
