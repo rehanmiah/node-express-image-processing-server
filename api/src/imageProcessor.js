5d9089220b68e5fb70b00fa36ff28eea97d0f13a
rehanmiah <rehan_miah@hotmail.com>
2021-01-29 21:09:57 +0000

    Update imageProcessor.js

--- api/src/imageProcessor.js
+++ api/src/imageProcessor.js
@@ -14,16 +14,15 @@ const imageProcessor = (filename =>  {
     const resizedDestination = uploadPathResolver('resized'+filename)
     const monochromeDestination = uploadPathResolver('monochrome-'+filename)
     
-
     return new Promise((resolve, reject) => {
-        if (isMainThread) {
-          try {
-            const resizeWorker = new Worker(pathToResizeWorker, {
-              workerData: {
-                source: sourcePath,
-                destination: resizedDestination,
-              },
-            });
+        if (isMainThread)
+        {}
+         else
+         {
+         reject(new Error('not on main thread'))
+         }
+         resolve()
+    } );
 
 
  });
