064c2d18b4ef9aae88d4fb61fdcbe07691013f51
rehanmiah <rehan_miah@hotmail.com>
2021-01-29 18:23:31 +0000

    Update resizeWorker.js

--- api/src/resizeWorker.js
+++ api/src/resizeWorker.js
@@ -3,4 +3,4 @@ const {parentPort, workerData} =require('worker_threads');
 
 gm(workerData.source)
 .resize(100,100)
-.write('workerData.destination',(request,response)=>{})
+.write(workerData.destination,(error)=>{})
