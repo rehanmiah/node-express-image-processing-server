2d9b97acf80f7175967fa092f29cbb80c4f75ff2
rehanmiah <rehan_miah@hotmail.com>
2021-01-29 20:50:40 +0000

    Update imageProcessor.js

--- api/src/imageProcessor.js
+++ api/src/imageProcessor.js
@@ -1,29 +1,3 @@
 const { response } = require('express');
 const path = require('path');
 const {worker, isMainThread} = require('worker_threads');
-
-const pathToResizeWorker = path.resolve('__dirname', 'resizeWorker.js');
-const pathToMonochromeWorker = path.resolve('__dirname', 'monochromeWorker.js');
-const food = uploadPathResolver(filename => {
-    path.resolve('__dirname', '../uploads', filname)
-} );
-
-const image = imageProcessor(filename =>  {
-    
-    const sourePath = uploadPathResolver(filename)
-    const resizedDestination = uploadPathResolver('resized-'(filename))
-    const monochromeDestination = uploadPathResolver('monochrome-'(filename))
-
-    new Promise((resolve, reject) => {
-        if (isMainThread)
-        {}
-         else
-         {
-         reject(new Error('not on main thread'))
-         }
-         resolve()
-    } )
-
-
- });
-//export module.exports;
