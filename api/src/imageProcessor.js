923ad0862651b1de9b388bdff9317cb8a38877d7
rehanmiah <rehan_miah@hotmail.com>
2021-01-30 21:46:55 +0000

    Update imageProcessor.js

--- api/src/imageProcessor.js
+++ api/src/imageProcessor.js
@@ -43,14 +43,18 @@ const uploadPathResolver = (filename) => {
                   });
 
                   
-
+/*
         resizeWorker.on('message', (message) => {
           resizeWorkerFinished = true;
           if (monochromeWorkerFinished) {
             resolve('resizeWorker finished processing');
           }
         });
-
+        */
+                resizeWorker.on('message',(message) => {
+                    resizedWorkerFinished = true;
+                    resolve('resizeWorker finished processing')
+                });
                 resizeWorker.on('error',(error) => {
                     reject(new Error(error.message))
                 });
