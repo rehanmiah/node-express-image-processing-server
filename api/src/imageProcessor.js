093e00e5f650a6e8c5f9db9b91533563e3d19252
rehanmiah <rehan_miah@hotmail.com>
2021-01-30 22:11:51 +0000

    Update imageProcessor.js

--- api/src/imageProcessor.js
+++ api/src/imageProcessor.js
@@ -79,7 +79,7 @@ const uploadPathResolver = (filename) => {
                   });
 
                   monochromeWorker.on('exit',(code) => {
-                      if(code!=0){
+                      if(code!==0){
                           reject(new Error('exited with status code '+ code))
                       }
                   });
