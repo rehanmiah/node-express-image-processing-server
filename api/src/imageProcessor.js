c368a68bb62bfe0579d1267df510b78c7cbdd8fb
rehanmiah <rehan_miah@hotmail.com>
2021-01-30 17:43:43 +0000

    Update imageProcessor.js

--- api/src/imageProcessor.js
+++ api/src/imageProcessor.js
@@ -11,10 +11,9 @@ const uploadPathResolver = (filename) => {
   
 const imageProcessor = (filename =>  {
     
-    const sourePath = uploadPathResolver(filename)
-    const resizedDestination = uploadPathResolver('resized-'+filename)
-    const monochromeDestination = uploadPathResolver('monochrome-'+filename)
-
+    const sourcePath = uploadPathResolver(filename);
+    const resizedDestination = uploadPathResolver('resized-' + filename);
+    const monochromeDestination = uploadPathResolver('monochrome-' + filename);
     return new Promise((resolve, reject) => {
         if (isMainThread)
         {
